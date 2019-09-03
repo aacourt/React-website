30f0f24963a347f3d98b3a8f2b12adb163edfcc7
Abby Acourt <Abigail.acourt@bglgroup.co.uk>
2019-09-03 14:00:49 +0100

    Update about me page

--- src/pages/about/index.js
+++ src/pages/about/index.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react';
+import './styles.css';
 
 class About extends Component {
   render() {
     return (
       <div >
-        <h1>Yo</h1>
+        <p>
+          Hello!
+          My name is Abby, I'm a Computer Science graduate, and Junior software engineer.
+        </p>
+        <p>
+          <img class=" size-medium wp-image-1345 aligncenter" src="https://acourtsthoughts.files.wordpress.com/2019/07/img_20190329_111623.jpg?w=450" alt="rptnb" width="225" height="300" />
+        </p>
       </div>
     );
   }
