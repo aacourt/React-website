954577f80cc625a49382175780235033a783d6f3
Abby Acourt <Abigail.acourt@bglgroup.co.uk>
2019-09-03 15:36:51 +0100

    Adding contact page

--- src/pages/contact/index.js
+++ src/pages/contact/index.js
@@ -1,11 +1,26 @@
 import React, { Component } from 'react';
+import './styles.css';
 
 class Contact extends Component {
   render() {
     return (
-      <div >
-        <h1>Yo contact</h1>
-      </div>
+      <div className="Contact">
+        <p>
+          If you ever want to get in contact with me , I have a few platforms for you to reach me on! So if there's anything you want me to write about, any questions you have or you just want to say hi, here's where to find me :
+        </p>
+        <p>
+          Twitter : acourtsthoughts
+        </p>
+        <p>
+          Instagram : acourtsthoughts
+        </p>
+        <p>
+          Email  : acourtsthoughts@hotmail.com
+        </p>
+        <p>
+          I look forward to hearing from you!
+        </p>
+      </div >
     );
   }
 }
